923fb0266d194156d6b4ecc8e099bf3e593af468
Vlad Bilyk <vlad.bilyk@gmail.com>
2014-11-01 01:00:22 +0300

    Project with Powershell snap-in is added

---
+++ Source/Ultramarine.Attribute.Powershell/UltramarineSnapIn.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel;
+using System.Management.Automation;
+
+namespace Ultramarine.Attribute.Powershell
+{
+    [RunInstaller(true)]
+    public class UltramarineSnapIn : PSSnapIn
+    {
+        public override string Name
+        {
+            get { return "UltramarineSnapIn"; }
+        }
+
+        public override string Vendor
+        {
+            get { return "Vlad Bilyk"; }
+        }
+
+        public override string Description
+        {
+            get { return "This is a PowerShell snap-in that includes the Ultramarine Attribute providr"; }
+        }
+    }
+}
