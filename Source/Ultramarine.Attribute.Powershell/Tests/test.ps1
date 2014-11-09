0d331d0c127314b73efdef3267070f5c962b1729
Vlad Bilyk <vlad.bilyk@gmail.com>
2014-11-09 13:10:00 +0300

    Test ps script was added

---
+++ Source/Ultramarine.Attribute.Powershell/Tests/test.ps1
@@ -0,0 +1,9 @@
+﻿Import-Module Ultramarine.Attribute.Powershell
+$imageDrive = Get-PSDrive -Name image
+if (!$imageDrive)
+{
+	New-PSDrive -Name image -PSProvider PhotoMetadata -Root "C:\Users\vbilyk\Pictures\Photo Stream\My Photo Stream\IMG_0022.JPG"
+}
+Get-Item image:\System.Photo.CameraModel
+Get-Item image:\System.Photo.CameraManufacturer
+Remove-PSDrive image
