9f3a8430243ed734fc7a2cfe03ef319ef1f287c2
Vlad Bilyk <vlad.bilyk@gmail.com>
2014-11-10 14:18:46 +0300

    Module publishing script was added

---
+++ Source/Ultramarine.Attribute.Powershell/publish-module.ps1
@@ -0,0 +1,8 @@
+﻿$modulePath = $env:PSModulePath.Split(";")[0]
+$ultramarineModule = Join-Path $modulePath "Ultramarine.Attribute.Powershell"
+if (!(Test-Path $ultramarineModule))
+{
+	New-Item -Path $ultramarineModule -ItemType Directory
+}
+$source = Join-Path $PSScriptRoot ".\bin\Release\*.dll"
+Copy-Item -Path $source -Destination $ultramarineModule -Exclude "System.*" -Force 
