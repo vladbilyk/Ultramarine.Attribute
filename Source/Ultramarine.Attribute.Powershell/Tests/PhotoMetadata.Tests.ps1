c6e01fd3971830dc730678e8754bff1dc66648b5
Vlad Bilyk <vlad.bilyk@gmail.com>
2014-11-11 02:16:57 +0300

    Pester tests were added

---
+++ Source/Ultramarine.Attribute.Powershell/Tests/PhotoMetadata.Tests.ps1
@@ -0,0 +1,16 @@
+﻿Import-Module Ultramarine.Attribute.Powershell
+
+Describe "PhotoMetadata provider tests" {
+    Context "Image drive" {
+        $imagePath = Join-Path $PSScriptRoot dsc_4646.jpg
+        New-PSDrive -Name image -PSProvider PhotoMetadata -Root $imagePath
+
+        It "Camera Model" {
+            Get-Item image:\System.Photo.CameraModel | Should Be "NIKON D300"
+        }
+
+        It "Camera Manufacturer" {
+            Get-Item image:\System.Photo.CameraManufacturer | Should Be "NIKON CORPORATION"
+        }
+    }
+}
--- Source/Ultramarine.Attribute.Powershell/Tests/test.ps1
+++
@@ -1,9 +0,0 @@
-﻿Import-Module Ultramarine.Attribute.Powershell
-if (!(Test-Path "image:"))
-{
-	$imagePath = Join-Path $PSScriptRoot dsc_4646.jpg
-	New-PSDrive -Name image -PSProvider PhotoMetadata -Root $imagePath
-}
-Get-Item image:\System.Photo.CameraModel
-Get-Item image:\System.Photo.CameraManufacturer
-Remove-PSDrive image
