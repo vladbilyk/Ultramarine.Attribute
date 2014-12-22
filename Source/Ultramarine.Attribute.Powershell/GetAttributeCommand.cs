92f66c13e4ce55a1f3edd73f6622b94658a6714a
Vlad Bilyk <vlad.bilyk@gmail.com>
2014-12-22 15:48:42 +0300

    Required parameter Path is added

--- Source/Ultramarine.Attribute.Powershell/GetAttributeCommand.cs
+++ Source/Ultramarine.Attribute.Powershell/GetAttributeCommand.cs
@@ -5,9 +5,17 @@ namespace Ultramarine.Attribute.Powershell
     [Cmdlet(VerbsCommon.Get, "Attribute")]
     public class GetAttributeCommand : Cmdlet
     {
+        [Parameter(
+             //Position = 0,
+             //ParameterSetName = "PatternParameterSet",
+             //ValueFromPipeline = true,
+             Mandatory = true)]
+        //[Alias("PSPath")]
+        public string Path { get; set; }
+
         protected override void ProcessRecord()
         {
-            WriteObject("Hello from cmdlet!");
+            WriteObject(Path);
         }
     }
 }
--- Source/Ultramarine.Attribute.Powershell/Tests/cmdlet.tests.ps1
+++ Source/Ultramarine.Attribute.Powershell/Tests/cmdlet.tests.ps1
@@ -3,8 +3,13 @@ Import-Module ..\bin\Release\Ultramarine.Attribute.Powershell.dll
 Describe "Cmdlet tests" {
     Context "Tests context" {
 
-        It "Hello" {
-            Get-Attribute | Should Be "Hello from cmdlet!"
+        It "Path is required" {
+            { Get-Attribute } | Should Throw "Cannot process command because of one or more missing mandatory parameters: Path."
         }
+
+        It "Dummy check of Path parameter" {
+            Get-Attribute -Path "Hello!" | Should Be "Hello!"
+        }
+
     }
 }
